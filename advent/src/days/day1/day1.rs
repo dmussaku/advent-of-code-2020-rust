478884b5636ede4bdba9dd53f4651066a6d5326a
Daniyar Mussakulov <dmussaku@gmail.com>
2021-02-26 11:33:25 +0100

    Add day1 solution

--- advent/src/days/day1/day1.rs
+++ advent/src/days/day1/day1.rs
@@ -21,8 +21,9 @@ Of course, your expense report is much larger. Find the two entries that sum to 
  what do you get if you multiply them together?
  */
 
-// use std::env;
 use std::fs;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub fn read_numbers_from_file(path: &str) -> Vec<i32>{
     let contents = fs::read_to_string(path)
@@ -33,9 +34,41 @@ pub fn read_numbers_from_file(path: &str) -> Vec<i32>{
     numbers
 }
 
-pub fn run() -> i8 {
-    read_numbers_from_file("src/days/day1/input_files/file.txt");
-    10
+pub fn run(input_numbers: Vec<i32>) -> i32 {
+
+    // We create a second vector by subtracting values from 2020
+    let transformed_numbers: Vec<_> = input_numbers.iter()
+        .map(|val| 2020 - *val)
+        .collect();
+
+    // Transform to HashSet
+    let input_set: HashSet<i32> = HashSet::from_iter(input_numbers.iter().cloned());
+    println!("Input Set");
+    for num in &input_set {
+        print!("{} ", num)
+    }
+    println!();
+
+    println!("Transformed set");
+    let transformed_set: HashSet<i32> = HashSet::from_iter(transformed_numbers.iter().cloned());
+    for num in &transformed_set{
+        print!("{} ", num)
+    }
+    println!();
+
+    let _intersection: HashSet<_> = input_set.intersection(&transformed_set).collect();
+    println!("Intersection");
+
+    for num in &_intersection{
+        print!("{} ", num);
+    }
+    println!();
+
+    let result: i32 = _intersection.iter()
+        .fold(1, |acc, val| acc * **val);
+
+    println!("Result = {}", result);
+    result
 }
 
 
@@ -44,8 +77,17 @@ mod tests{
     use super::*;
 
     #[test]
-    fn this_test_will_pass() {
-        assert_eq!(10, run());
+    fn test_read_input_numbers() {
+        let input_numbers = read_numbers_from_file(
+            "src/days/day1/input_files/test_file.txt"
+        );
+
+        assert_eq!(vec![1721, 979, 366, 299, 675, 1456], input_numbers);
+    }
+
+    #[test]
+    fn test_run() {
+        assert_eq!(514579, run(vec![1721, 979, 366, 299, 675, 1456]));
     }
 }
 
--- advent/src/main.rs
+++ advent/src/main.rs
@@ -3,5 +3,6 @@ mod days;
 use days::{day1};
 
 fn main() {
-    day1::day1::run();
+    let day1_input = day1::day1::read_numbers_from_file("src/days/day1/input_files/file.txt");
+    day1::day1::run(day1_input);
 }
